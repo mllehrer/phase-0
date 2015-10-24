9bbee3d6b411f462b1c7805fea4b3918cafdff18
mllehrer <mia.lehrer@gmail.com>
2015-10-24 12:06:58 -0700

    Complete 4.2

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -22,9 +22,26 @@ def better_number
   puts "Interesting. Well, if you get a call from a government agency in the near future, don't tell them about this."
 end
 better_number
-#--How do you define a local variable?
-How do you define a method?
-What is the difference between a local variable and a method?
-How do you run a ruby program from the command line?
-How do you run an RSpec file from the command line?
-What was confusing about this material? What made sense?
+
+# Address link: https://github.com/mllehrer/phase-0/tree/master/week-4/address
+
+#Math link: https://github.com/mllehrer/phase-0/tree/master/week-4/math
+
+#How do you define a local variable?
+#A variable that is assigned within a method (or a class, etc.) and is accessible only within the bounds it was assigned to. It must be assigned again outside the method to be accessible there.
+
+#How do you define a method?
+#A method is a defined block that is give a name and can be called by that name to run all the code within the method. I wrote a program called "Dave" that imitates the way our office maintenance guy interrogates us when we break things, and by calling "Dave" from the command line, people can go through an entire interaction with him.
+
+#What is the difference between a local variable and a method?
+#It's almost exactly the difference between a noun and a verb. A local variable is an object on which a method can act. The variable doesn't "do" anything--it can have things done to it. A method is an action or set of actions that are performed on an object. Best illustrated with an example:
+# string = "plz capitalize me." (string is the local variable)
+# string.capitalize! (capitalize is the method that acts on the string we have stored as string. The ! will make the change permanent.)
+# The program will return "Plz capitalize me."
+
+#How do you run a ruby program from the command line?
+# "ruby rubyfile.rb"
+#How do you run an RSpec file from the command line?
+# "rspec thespecfile.rb"
+#What was confusing about this material? What made sense?
+#I had never used rspec before, so I had a little trouble figuring that one out. I'm just so happy to be working in ruby again and learning more. I had done the codecademy course but I actually think I can solve problems a lot better now than I could the last time I was writing ruby.
