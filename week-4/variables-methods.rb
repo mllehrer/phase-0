fa6dab7bc511c704600f56bb7024017f11636a98
mllehrer <mia.lehrer@gmail.com>
2015-10-23 18:32:08 -0700

    modify 4.3 file

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -22,3 +22,9 @@ def better_number
   puts "Interesting. Well, if you get a call from a government agency in the near future, don't tell them about this."
 end
 better_number
+#--How do you define a local variable?
+How do you define a method?
+What is the difference between a local variable and a method?
+How do you run a ruby program from the command line?
+How do you run an RSpec file from the command line?
+What was confusing about this material? What made sense?
