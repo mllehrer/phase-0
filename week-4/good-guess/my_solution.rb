011b2111bb8a614118512c1a67ecc12076acf2c4
mllehrer <mia.lehrer@gmail.com>
2015-10-24 20:37:00 -0700

    4.5.1

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,19 @@
+# Good Guess
+
+# I worked on this challenge with Alicia Quezada.
+
+
+# Your Solution Below
+def good_guess?(number)
+  if number == 42
+    p true
+  else
+    p false
+  end
+end
+
+#Pseudocode
+#Input: integer
+#Output: boolean
+#if 42, true
+#if not 42, false
