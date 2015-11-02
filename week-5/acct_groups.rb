18ca9fed4364e578790a8659ea4df57ea07fa122
mllehrer <mia.lehrer@gmail.com>
2015-11-01 22:56:36 -0800

    work on acct_groups

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -8,9 +8,9 @@
 # Fill each array with the first four people, then the next four, etc.
 
   def group(ppl)
-    @all = ppl.sort
+    @all = ppl.sort_by {rand}
     @count = ppl.count
   end
   group(["Syema Ailia", "Alan Alcesto", "Daniel Andersen", "James Artz", "Darius Atmar", "Brian Bensch", "Nicola Beuscher", "Kris Bies", "Logan Bresnahan", "William Brinkert", "Scott Chou", "Bernice Anne W Chua", "Abraham Clark", "Jon Clayton", "Kevin Corso", "Jacob Crofts", "Amaar Fazlani", "Solomon Fernandez", "Edward Gemson", "Jamar Gibbs", "Chris Gomes", "Will Granger", "Christopher M. Guard", "Ryan Ho", "Igor Kazimirov", "Walter Kerr", "Karla King", "Becky Lehmann", "Malia Lehrer", "Carolina Medellin", "Timothy Meixell", "Lorena Mesa", "Chris Miklius", "Joshua Monzon", "Shea Munion", "Bryan Munroe", "Neal Peters", "Trevor Newcomb", "Aleksandra Nowak", "Fatma Ocal", "Van Phan", "Luis Fernando Plaz", "Natalie Polen", "Alicia Quezada", "Jessie Richardson", "Nimi Samocha", "Zach Schatz", "Tal Schwartz", "Pratik Shah", "Josh Shin", "Shawn Spears", "Sasha Tailor", "Nil Thacker", "Natasha Thapliyal", "Sabrina Unrein", "Brian Wagner", "Clinton Weber", "Gregory Wehmeier", "Michael Whelpley", "Alexander Williams", "Peter N Wood", "Ryan Zell"])
 
-  p @all.each_slice(4).map { |x| [x]  }
+  p @all.each_slice(4).map { |x| puts "#{x.join(", ")} are in a group together."}
