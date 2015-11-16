04d341d446b43e03460fe5445a6feb91e70cd751
mllehrer <mia.lehrer@gmail.com>
2015-11-15 21:31:39 -0800

    7.2 and 7.5

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -1,42 +1,3 @@
-/*Introduction
-
-I really liked the example in the text that took a simple program from binary into JavaScript. I hadn't seen it so clearly explained before. JavaScript is all new to me, and the author has an engaging writing style in describing its history. I'm a little nervous about moving from Ruby into a program whose primary purpose is running in a browser.
-
-Ch.1
-Similarities and Differences:
-Similarity: Both JavaScript and Ruby have a type of data on which they perform arithmetic operations
-Difference: JavaScript does not differentiate integers from "floats" the way Ruby does.
-Similarity: Both languages allow the storage of data in variables
-Difference: in order to store something in a variable in JavaScript, you can't just set it to equal. You have to write "var" in front of it.
-Bit difference: JavaScript will actually try to run any program you give it, so unlike Ruby where you can't multiply a string and a number, JavaScript will try to do it. However, since you can concatenate strings, JavaScript will concatenate a number and a string instead of adding them. It's called "type coercion" and Ruby definitely doesn't have it.
-
-Ch.2
-What is an expression?
-  An expression is a fragment of code that produces a value.
-
-What is the purpose of semicolons in JavaScript? Are they always required?
-  Semicolons mark the end of statements. There are some cases in which they are not required. We have not learned them yet.
-
-What causes a variable to return undefined?
-  If the variable has not been assigned to anything yet, its value is undefined and will return such.
-
-What does console.log do and when would you use it? What Ruby method(s) is this similar to?
-console.log prints the results of the program to the console. I think it's closes to "p", since it seems to print numbers when given numbers, instead of a string.
-
-Write a short program that asks for a user to input their favorite food. After they hit return, have the program respond with "Hey! That's my favorite too!" (You will probably need to run this in the Chrome console (Links to an external site.) rather than node since node does not support prompt or alert). Paste your program into the eloquent.js file.
-
-Describe while and for loops
-  While loops execute as long as certain conditions are met (as specified by the "while"). For loops execute only during the conditions specified at the beginning. They're really a type of while loop that is more specific.
-
-What other similarities or differences between Ruby and JavaScript did you notice in this section?
-  I think the biggest difference for me is that I can run JavaScript programs right in my browser and actually see the results on the page, instead of just within the terminal window. Ruby's actual code looks a lot nicer to me, though; in Ruby I feel like I'm reading and writing in English, and just talking to someone who learned it as a second language, but in JavaScript I feel more like I'm speaking broken Computerese.
-
-What are the differences between local and global variables in JavaScript?
-When should you use functions?
-What is a function declaration?
-Complete the minimum exercise in the eloquent.js file.
-*/
-
 // Eloquent JavaScript
 
 // Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
@@ -73,7 +34,23 @@ while (counter < 8) {
 
 // Complete the `minimum` exercise.
 
+function min(num1, num2) {
+var num1 = num1;
+var num2 = num2;
+
+  if (num1 < num2)
+      return num1
+    else
+      return num2 }
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+me = {
+  name: "Malia Lehrer"
+  age: 26
+  favoriteFood1: "Chocolate"
+  favoriteFood2: "Sushi"
+  favoriteFood3: "BBQ Ribs"
+};
--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,20 +1,65 @@
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
-
 // I worked on this challenge with: Syema Ailia.
 
 // Pseudocode
-//Reverse the number
-//Separate the number into groups of three
-//Until
+//Make the number into a string
+//Make the string into an array
+//Set an index variable equal to the length of the array
+//If the length is <4
+//Return the number
+//Else
+//Subtract three from the index each loop While
+//the index is greater than 0
+//Add a comma each loop
+//Join the array into a string
+//Return the string
 
 
 // Initial Solution
+
+// var num = 800000;
+
+//     var numAsString = num.toString().split('');
+//     var numIndex = numAsString.length;
+//     if (numIndex < 4) {
+//       console.log(num);
+//     }
+
+
+//     var index = numIndex;
+//     var numArray = numAsString;
+
+//     index -= 3;
+
+//      while (index > 0) {
+//      numArray.splice(index, 0, ",");
+//      index -= 3;
+//       }
+//      numArray = numArray.join('');
+//       console.log(numArray);
 
 
 
 
 // Refactored Solution
+
+var num = 80000;
+
+    var numArray = num.toString().split('');
+    var numIndex = numArray.length;
+    if (numIndex < 4) {
+      console.log(num);
+    }
+
+    numIndex -= 3;
+
+     while (numIndex > 0) {
+     numArray.splice(numIndex, 0, ",");
+     numIndex -= 3;
+      }
+     numArray = numArray.join('');
+      console.log(numArray);
 
 
 
@@ -25,3 +70,15 @@
 
 
 // Reflection
+/* What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+  It was very frustrating overall because Ruby is much cleaner and easier to use than JavaScript. The Coderpad JS interface is also kind of buggy, so we ended up having to screenshare, which is much slower than Coderpad was for Ruby.
+
+What did you learn about iterating over arrays in JavaScript?
+  It seems like JS and Ruby treat arrays about the same, so that's nice. JS tends to have extra things to do (like in splice, you have to specify that you don't want something to be removed).
+
+What was different about solving this problem in JavaScript?
+  All the semicolons! Syntax is pretty clunky and confusing.
+
+What built-in methods did you find to incorporate in your refactored solution?
+  We ended up using the splice method, which is similar to the one we used in the Ruby version of this challenge as well.
+*/
