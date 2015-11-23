bde0803864a7cee78af162a84db149d555d40be0
mllehrer <mia.lehrer@gmail.com>
2015-11-22 20:16:18 -0800

    edits

--- week-7/game.js
+++ week-7/game.js
@@ -119,6 +119,109 @@ function takeItem(item){
 
 
 // Refactored Code
+/* I couldn't figure out how to make it run by the deadline, but after it was due I had a friend who understands JavaScript better help me understand what I did wrong; this violates the solo challenge rules which is why I want to declare that in advance. This one runs, so I want to include it for posterity.
+
+var bagTypes = ["duffel", "backpack", "belt bag"];
+
+var tools = ["shovel", "GPS", "blaster", "oxygen mask"];
+
+var yourBag = {
+};
+
+var startGame = function() {
+alert("You are flying through space, and have been for some time. Through the windows of your spacecraft, the stars, which had been a blur as you whirred through the cosmos at hyperspeed, have begun to resolve into the wash of glitter that has been your only sky for five years. You are slowing down, planning to enter the orbit of an earthlike planet that has been your destination: it is humanity's only hope for a new world.");
+
+alert("Suddenly, your spacecraft begins to shake");
+
+alert("There is an unexpected gravitational anomaly! You lose control of your navigation system and begin plummeting to the surface of an alien planet!");
+
+//code here to open a new window with a pretty picture if I get time
+
+alert("...");
+
+var r = confirm("You are awake. You are alive. From the rubble of your spacecraft, you hear the sound of an alien wind. Astronaut, are you ready?");
+    if (r === false) {
+        alert("You were not ready, but you are far from home. The atmosphere is not as oxygen-rich as Earth's, and you die slowly but painlessly of asphyxiation.");
+       return;
+    } else {
+        var bag = prompt("You will need supplies. Choose a bag. You have a duffel that is heavy but can hold three items, a backpack that is lighter and can hold two, and a belt bag that will not weigh you down at all but can only hold one thing. Which will you choose? Input \"duffel\", \"backpack\", or \"belt bag\".");
+      yourBag.bagType = bag;
+      alert("You have chosen the " + bag + ".");
+     }
+
+  var firstTool = prompt("There are a few tools left in the spacecraft. You can take up to three if you chose the duffel, two if you chose the backpack, and one if you chose the belt bag. The tools are a shovel, a gps, a blaster gun, and an oxygen mask. To fill your bag, input 'shovel', 'gps', 'blaster', or 'oxygen mask'.");
+yourBag.items = [];
+yourBag.items.push(firstTool);
+
+alert("The " + firstTool + " has been added to your bag.");
+
+  if (yourBag.bagType === "duffel"){
+    var secondTool = prompt("You may choose two more tools. What is your next choice? Remember the options are 'shovel', 'gps', 'blaster', or 'oxygen mask'.");
+    yourBag.items.push(secondTool);
+    alert("The " + secondTool + " has been added to your bag.");
+    var thirdTool = prompt("You may choose one more tool. What will it be? Remember the options are 'shovel', 'gps', 'blaster', or 'oxygen mask'.");
+    yourBag.items.push(thirdTool);
+    alert("The " + thirdTool + " has been added to your bag.");
+  } else if (yourBag.bagType === "backpack"){
+      var secondTool = prompt("You may choose one more tool. What will it be? Remember the options are 'shovel', 'gps', 'blaster', or 'oxygen mask'.");
+    yourBag.items.push(secondTool);
+    alert("The " + secondTool + " has been added to your bag.");
+    } else {
+    alert("Wow, you must be very brave. I hope that was the right choice.");
+  }
+
+  alert("You begin walking. Around you is lush forest of strange plants with waxy leaves. Something small darts into the bushes. You are not alone.");
+
+  alert("As you lose sight of the smoking, twisted heap of what used to be your spacecraft, you begin to feel lightheaded.");
+
+
+  if (yourBag.items.indexOf("oxygen mask") > -1){
+     alert("It's probably a good idea to put on your oxgen mask.");
+  } else {
+    alert("Oh dear, looks like you didn't bring an oxygen mask. You die of asphyxiation.");
+    return;
+  }
+
+    r = confirm("Breathing normally again, you take stock of your surroundings. The only way forward seems to be through a clearing, but the ground looks soft and boggy. Do you risk it?");
+    if (r === false){
+      alert("Staying in one place gets you eaten by an alien carnivore. You never saw it coming.");
+     return;
+    } else {
+      alert("Brave explorer! You trek ahead.");
+    }
+
+    if (yourBag.bagType === "duffel"){
+      alert("The soft ground gives way under your feet. You try to climb out but your bag is too heavy! You perish in the quicksand.");
+      return;
+    } else {
+      r = confirm("The ground is soft under your feet, and you flounder a bit but manage to cross into a wide meadow carpeted with bright flowers. A structure sits at the far end of the clearing. It looks as if it were built by something. Do you approach?");
+      if (r === false){
+      alert("Prudent, but a bad idea. You wander around in the forest until you die of starvation.");
+      return;
+    } else {
+      alert("Something stirs in the windows of the building. A set of tentacles ooze out of the door, followed by a head with large, intelligent eyes. It looks like a terrestrial squid, and it is moving toward you.");
+    }
+   }
+
+    if (yourBag.items.indexOf("blaster") > -1){
+      r = confirm("You have your blaster. Do you want to shoot it?");
+      if (r === true) {
+        alert("What is wrong with you? Game over. Close your laptop and feel guilty for a little while. It only wanted to help.");
+        return;
+      } else {
+        alert("Your pacifism is rewarded. The land-squids adopt you and teach your their sign language, since you cannot understand their telepathy. You teach them to read. Eventually they bear you to their capitol city where the land-squid scientists are able to craft an emergency beacon and get you rescued by other humans in a few years. The other humans like the new planet so much they stay and you form a peaceful community.");
+      }
+    } else {
+      alert("It's a strange alien, but a friendly one.  The land-squids adopt you and teach your their sign language, since you cannot understand their telepathy. You teach them to read. Eventually they bear you to their capitol city where the land-squid scientists are able to craft an emergency beacon and get you rescued by other humans in a few years. The other humans like the new planet so much they stay and you form a peaceful community.");
+    }
+
+alert("You Won! Game over!");
+
+};
+
+startGame()
+
+*/
 
 
 
