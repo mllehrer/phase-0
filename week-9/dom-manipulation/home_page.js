d719fa192e489f363230dedabd03bcca9e5a69e7
mllehrer <mia.lehrer@gmail.com>
2015-11-27 08:49:56 -0800

    9.3

---
+++ week-9/dom-manipulation/home_page.js
@@ -0,0 +1,38 @@
+// DOM Manipulation Challenge
+
+
+// I worked on this challenge [by myself, with: ].
+
+
+// Add your JavaScript calls to this page:
+
+// Release 0:
+document.getElementById('release-0').classList.add('done');
+
+
+
+// Release 1:
+document.getElementById("release-1").style.display = "none";
+
+
+
+
+// Release 2:
+document.getElementById("release-3").style.backgroundColor = #955251;
+
+
+
+
+// Release 3:
+
+
+
+
+// Release 4:
+document.getElementsByClassName("release-4").style.fontSize = "2em";
+
+
+
+// Release 5:
+document.getElementById("hidden").style.visibility = "visible";
+
